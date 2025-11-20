67be7751503403dd363bdc7261bddc072c4a9422
hipetech <kirillko20002@gmail.com>
2025-11-20 22:31:27 +0200

    Add yarn.lock file for dependency management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,43 +6,47 @@ import {
   VectorIcon,
 } from "expo-router/unstable-native-tabs";
 import { Platform } from "react-native";
+import {PortalHost} from "@rn-primitives/portal"
 
 import "../global.css";
 
 export default function TabLayout() {
   return (
-    <NativeTabs>
-      <NativeTabs.Trigger name="workouts">
-        <Label>Workouts</Label>
-        {Platform.select({
-          ios: <Icon sf="dumbbell.fill" />,
-          android: (
-            <Icon
-              src={<VectorIcon family={MaterialIcons} name="fitness-center" />}
-            />
-          ),
-        })}
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="exercises">
-        <Label>Exercises</Label>
-        {Platform.select({
-          ios: <Icon sf="figure.run" />,
-          android: (
-            <Icon
-              src={<VectorIcon family={MaterialIcons} name="directions-run" />}
-            />
-          ),
-        })}
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="settings">
-        <Label>Settings</Label>
-        {Platform.select({
-          ios: <Icon sf="gear" />,
-          android: (
-            <Icon src={<VectorIcon family={MaterialIcons} name="settings" />} />
-          ),
-        })}
-      </NativeTabs.Trigger>
-    </NativeTabs>
+    <>
+      <NativeTabs>
+        <NativeTabs.Trigger name="workouts">
+          <Label>Workouts</Label>
+          {Platform.select({
+            ios: <Icon sf="dumbbell.fill" />,
+            android: (
+              <Icon
+                src={<VectorIcon family={MaterialIcons} name="fitness-center" />}
+              />
+            ),
+          })}
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="exercises">
+          <Label>Exercises</Label>
+          {Platform.select({
+            ios: <Icon sf="figure.run" />,
+            android: (
+              <Icon
+                src={<VectorIcon family={MaterialIcons} name="directions-run" />}
+              />
+            ),
+          })}
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="settings">
+          <Label>Settings</Label>
+          {Platform.select({
+            ios: <Icon sf="gear" />,
+            android: (
+              <Icon src={<VectorIcon family={MaterialIcons} name="settings" />} />
+            ),
+          })}
+        </NativeTabs.Trigger>
+      </NativeTabs>
+      <PortalHost />
+    </>
   );
 }
