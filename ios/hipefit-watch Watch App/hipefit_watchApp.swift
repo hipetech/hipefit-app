c86710b2b0d9df9d3c558180f312690942ec42f4
hipetech <kirillko20002@gmail.com>
2025-11-30 21:48:56 +0200

    Add watchOS app support for Hipefit with environment-specific configurations
    - Add native watchOS target (`hipefit-watch`), alongside `hipefit-watch-dev` and `hipefit-watch-stage`.
    - Include new `ContentView.swift` and app entry file for watchOS.
    - Update `.pbxproj` with build phases, embedded content, and related configurations.
    - Add environment-specific schemes for streamlined development and staging workflows.

---
+++ ios/hipefit-watch Watch App/hipefit_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  hipefit_watchApp.swift
+//  hipefit-watch Watch App
+//
+//  Created by Kyrylo Korota on 30.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct hipefit_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
